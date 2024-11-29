4a81b7e6585e9eb7c5994efcd038aa9f9c0189c8
Misery-aripes <aaaa_wwww555@mail.ru>
2024-11-29 16:20:58 +0500

    new Project

---
+++ src/main/java/ru/yandex/practicum/CatsgramApp.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsgramApp {
+    public static void main(final String[] args) {
+        SpringApplication.run(CatsgramApp.class, args);
+    }
+}
+
+
